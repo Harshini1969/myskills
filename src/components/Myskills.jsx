893d6bf43ccaffe82a25097e5a84e69dc95ced9c
guna <gunanarahari@gmail.com>
2026-01-30 20:06:00 +0530

    Final

--- src/components/Myskills.jsx
+++ src/components/Myskills.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 import "./myskills.css";
 
-function Myskills() {
-
-
-  let  skillList = Myskills.map((item, index) => (
-    <div className="skill" key={index}>
-      <div className="info">
-        <h3>{item.name}</h3>
-        <p>{item.level}%</p>
-      </div>
-
-      <div className="a">
-        <div
-          className={`b ${item.name.toLowerCase()}`}
-          style={{ width: `${item.level}%` }}
-        />
-      </div>
-    </div>
-  ))
+function Skills() {
+  let myskills = [
+    { name: "HTML", level: 90 },
+    { name: "CSS", level: 85 },
+    { name: "JavaScript", level: 80 },
+    { name: "React", level: 75 },
+    { name: "Node", level: 70 },
+    { name: "Python", level: 65 },
+    { name: "MongoDB", level: 60 },
+    { name: "TypeScript", level: 55 },
+  ];
 
   return (
     <div className="container">
@@ -27,20 +20,31 @@ function Myskills() {
       <p className="description">
         Mastering the full spectrum of web technologies, I seamlessly integrate
         front-end and back-end development to create cohesive, high-performing
-        applications that deliver exceptional user experiences and meet diverse
-        business needs.
+        applications.
       </p>
 
       <div className="grid">
-    
-             {skillList}
+        {myskills.map((item, index) => (
+          <div className="skill" key={index}>
+            <div className="info">
+              <h3 className="skill-name">
+                <span className="skill-icon">{item.icon}</span>
+                {item.name}
+              </h3>
+              <p>{item.level}%</p>
+            </div>
 
-        <div className="text">
-          <h1>WORKS</h1>
-        </div>
+            <div className="a">
+              <div
+                className={`b ${item.name.toLowerCase()}`}
+                style={{ width: `${item.level}%` }}
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-export default Myskills;
+export default Skills;
